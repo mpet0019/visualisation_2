66fac37baf8df9688fde5135ca93ea201cdca045
mpet0019 <144070387+mpet0019@users.noreply.github.com>
2023-10-17 01:43:14 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var vg_1 = "bubbles.json";
+vegaEmbed("#bubble", vg_1,{"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "language_scatter.json";
+vegaEmbed("#language_scatter", vg_2,{"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "aus_pie.json";
+vegaEmbed("#aus_pie", vg_3,{"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "world_pie.json";
+vegaEmbed("#world_pie", vg_4,{"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "map.vl.json";
+vegaEmbed("#map", vg_5,{"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "word_count.json";
+vegaEmbed("#word_count", vg_6,{"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
